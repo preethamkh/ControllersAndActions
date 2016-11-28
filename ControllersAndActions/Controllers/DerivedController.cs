3b56f68f10866d10c50d47290e95352ee0777638
preethamkh <blighter.soul@gmail.com>
2016-11-28 16:43:27 +1100

    redirects

---
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllersAndActions.Infrastructure;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+
+        public ActionResult ProduceOutput()
+        {
+            // custom redirection
+            //if(Server.MachineName == "TEST")
+            //{
+            //    return new CustomRedirectResult { Url = "/Basic/Index" };
+            //}
+            //else
+            //{
+            //    Response.Write("Controller: Derived, Action: ProduceOutput");
+            //    return null;
+            //}
+
+            return new RedirectResult("/Basic/Index");
+        }
+    }
+}
